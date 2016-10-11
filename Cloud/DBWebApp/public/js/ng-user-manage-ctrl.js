0eb069992c7dade948c0f3c873fca2475aeb5438
fookwin <fookwin@126.com>
2016-10-12 00:01:00 +0800

    limited table height and set default selection on select

--- Cloud/DBWebApp/public/js/ng-user-manage-ctrl.js
+++ Cloud/DBWebApp/public/js/ng-user-manage-ctrl.js
@@ -1,5 +1,6 @@
 angular.module('ng-index-app').controller('ng-user-manage-ctrl', function ($scope, $rootScope, $timeout, $http) {
     $rootScope.selectedNavIndex = 1;
+    $scope.inLoadingData = false;
     $scope.platformList = [
         {name: 'Windows Store', index: '1'},
         {name: 'Windows Phone', index: '2'},
@@ -12,12 +13,17 @@ angular.module('ng-index-app').controller('ng-user-manage-ctrl', function ($scop
         {name: 'in one month', scope: '30'}
         ];
 
+    $scope.selectedPlatform = $scope.platformList[0];
+    $scope.selectedScope = $scope.scopeList[0];
+
     $scope.refreshTable = function() {
         
         if ($scope.selectedPlatform !== undefined && $scope.selectedScope !== undefined) {
+            $scope.inLoadingData = true;
             $http.get('/users/?platform=' + $scope.selectedPlatform.index + '&scope=' + $scope.selectedScope.scope).success(function (res) {
                $scope.userList = res.data;
                $scope.countOfResult = $scope.userList.length; 
+               $scope.inLoadingData = false;
             });
         }
     }  
