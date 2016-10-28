326461e501fcaf150ba8f0f62984009ef0b9f2f2
zhangze <zengxu.zhang@autodesk.com>
2016-10-28 18:23:29 +0800

    remove web app from this repository

--- Cloud/DBWebApp/public/js/ng-home-ctrl.js
+++
@@ -1,3 +0,0 @@
-angular.module('ng-index-app').controller('ng-home-ctrl', function ($scope, $rootScope, $timeout, $http) {
-    $rootScope.selectedNavIndex = 0;
-});
--- Cloud/DBWebApp/routes/index.js
+++
@@ -1,6 +0,0 @@
-﻿
-//var path = require('path');
-
-exports.index = function (req, res) {
-    res.render('index.html');
-};
