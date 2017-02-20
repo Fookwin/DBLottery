840d38c84a921b79c94ed0f17a2a565470b8847b
fookwin <fookwin@126.com>
2017-02-20 17:38:33 +0800

    add qq splash ad

--- Android_Studio/app/src/main/java/com/fookwin/lotteryspirit/WelcomeActivity.java
+++ Android_Studio/app/src/main/java/com/fookwin/lotteryspirit/WelcomeActivity.java
@@ -10,6 +10,9 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.qq.e.ads.splash.SplashAD;
+import com.qq.e.ads.splash.SplashADListener;
+import com.qq.e.comm.constants.Constants;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -21,6 +24,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -29,7 +34,7 @@ import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class WelcomeActivity extends Activity {
+public class WelcomeActivity extends Activity implements SplashADListener {
 	private int screenHeight;
 	private int screenWidth;
 	private ImageView welcome_imageview;
@@ -38,7 +43,7 @@ public class WelcomeActivity extends Activity {
 	private View skipad_button;
 
 	private boolean dataLoaded = false;
-	private boolean finished = false;
+	private boolean adClosed = false;
 
 	@SuppressLint("HandlerLeak")
 	Handler hannext = new Handler() {
@@ -69,6 +74,8 @@ public class WelcomeActivity extends Activity {
 	 * See https://g.co/AppIndexing/AndroidStudio for more information.
 	 */
 	private GoogleApiClient client;
+	private TextView skipView;
+	private SplashAD splashAD;
 
 	/**
 	 * Called when the activity is first created.
@@ -93,7 +100,12 @@ public class WelcomeActivity extends Activity {
 		screenHeight = dm.heightPixels;
 		screenWidth = dm.widthPixels;
 
+		ViewGroup container = (ViewGroup) this.findViewById(R.id.ad_view);
+		skipView = (TextView) findViewById(R.id.skip_ad_button);
+		fetchSplashAD(this, container, skipView, "1102491872", "7000604514947293", this, 0);
+
 		init();
+
 		// ATTENTION: This was auto-generated to implement the App Indexing API.
 		// See https://g.co/AppIndexing/AndroidStudio for more information.
 		client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
@@ -162,13 +174,12 @@ public class WelcomeActivity extends Activity {
 	}
 
 	private void gotoMainPage() {
-		if (!finished) {
+		if (adClosed && dataLoaded) {
 			finish();
 			Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
 			startActivity(intent);
 
 			skipad_button.setEnabled(false);
-			finished = true;
 		}
 	}
 
@@ -211,5 +222,72 @@ public class WelcomeActivity extends Activity {
 		// See https://g.co/AppIndexing/AndroidStudio for more information.
 		AppIndex.AppIndexApi.end(client, getIndexApiAction());
 		client.disconnect();
+	}
+
+	/**
+	 * 拉取开屏广告，开屏广告的构造方法有3种，详细说明请参考开发者文档。
+	 *
+	 * @param activity        展示广告的activity
+	 * @param adContainer     展示广告的大容器
+	 * @param skipContainer   自定义的跳过按钮：传入该view给SDK后，SDK会自动给它绑定点击跳过事件。SkipView的样式可以由开发者自由定制，其尺寸限制请参考activity_splash.xml或下面的注意事项。
+	 * @param appId           应用ID
+	 * @param posId           广告位ID
+	 * @param adListener      广告状态监听器
+	 * @param fetchDelay      拉取广告的超时时长：取值范围[3000, 5000]，设为0表示使用广点通SDK默认的超时时长。
+	 */
+	private void fetchSplashAD(Activity activity, ViewGroup adContainer, View skipContainer,
+							   String appId, String posId, SplashADListener adListener, int fetchDelay) {
+		splashAD = new SplashAD(activity, adContainer, skipContainer, appId, posId, adListener, fetchDelay);
+	}
+
+	@Override
+	public void onADPresent() {
+		Log.i("AD_DEMO", "SplashADPresent");
+		welcome_imageview.setVisibility(View.INVISIBLE); // 广告展示后一定要把预设的开屏图片隐藏起来
+	}
+
+	@Override
+	public void onADDismissed() {
+		Log.i("AD_DEMO", "SplashADDismissed");
+		next();
+	}
+
+	private void next() {
+		adClosed = true;
+
+		gotoMainPage();
+	}
+
+	@Override
+	public void onNoAD(int arg0) {
+		Log.i("AD_DEMO", "LoadSplashADFail,ecode=" + arg0);
+		next();
+	}
+
+
+	@Override
+	public void onADClicked() {
+		Log.i("AD_DEMO", "SplashADClicked");
+	}
+
+	/**
+	 * 倒计时回调，返回广告还将被展示的剩余时间。
+	 * 通过这个接口，开发者可以自行决定是否显示倒计时提示，或者还剩几秒的时候显示倒计时
+	 *
+	 * @param millisUntilFinished 剩余毫秒数
+	 */
+	@Override
+	public void onADTick(long millisUntilFinished) {
+		Log.i("AD_DEMO", "SplashADTick " + millisUntilFinished + "ms");
+		skipView.setText(String.format("剩%1f秒", Math.round(millisUntilFinished / 1000f)));
+	}
+
+	//防止用户返回键退出APP
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_HOME) {
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
 	}
 }
